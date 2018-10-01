11c87b05774784d9a09b531fdcedf4a3adce02fe
Nishant Arya <nishantarya98@gmail.com>
2018-10-01 23:04:23 +0530

    Fix calibrate.sh

--- calibrate.sh
+++ calibrate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-new_name=$(python csv_to_json_log.py $1)
+new_name=$(python gen_params.py $1)
 cd ../calibration/scripts
 ./run_all.sh |& tee ../../"$new_name"/"calibration_params.txt"
